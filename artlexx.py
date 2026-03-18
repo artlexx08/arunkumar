7544524fbfbcddcb97dcbbc128dc6cea6e26b42c
artlexx08 <arun108208@gmail.com>
2026-03-18 23:01:35 +0530

    firstcommit

---
+++ artlexx.py
@@ -0,0 +1,20 @@
+print(" Temperature Conversion :")
+print("Convert Celsius to Farenhiet")
+d  = input("Enter the Celsius :")
+c = (int(d))
+f = (c * 9/5) + 32
+print(f)
+
+print("Weight Conversion :")
+print("Kilograms to pound ")
+a = input("Enter the kilograms :")
+k = (int(a))
+p  = k  * 2.20462
+print(p)
+
+print("Distance Conversion :")
+print("Kilometers to Miles")
+m = input("Enter the Kilograms :")
+k = (int(m))
+M = k*0.621371
+print(M)
